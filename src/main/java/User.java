0b6fc73e1339bd7261ab0bd06d4e552cfc43e3ea
BrayaMDev <cg4463380@gmail.com>
2026-03-18 17:44:25 -0600

    feat: agregar lógica y métodos básicos a clase User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,24 @@
 public class User {
-    
+    private final Email email;
+    private final String password;
+
+    public User(Email email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email=" + email +
+                '}';
+    }
 }
