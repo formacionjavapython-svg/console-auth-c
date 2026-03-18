2f063b4079fca6263ceaa4c0a8ad1c6d43d0883d
BrayaMDev <cg4463380@gmail.com>
2026-03-18 17:34:39 -0600

    feat: agregar validación básica a clase Email

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Email {
+    private final String value;
+
+    public Email(String value) {
+        if (!isValid(value)) {
+            throw new IllegalArgumentException("Email inválido");
+        }
+        this.value = value;
+    }
+
+    private boolean isValid(String value) {
+        return value != null && value.contains("@") && value.contains("."); //Evaluación de email o parametros que debe cumplir el correo
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
