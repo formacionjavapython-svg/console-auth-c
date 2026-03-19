e7a9f67f392a4682b50b61be4556163768330db6
irvin_farfan <irvin.farfano@gmail.com>
2026-03-19 08:51:12 +0800

    feat: implementar validación de política y hashing PBKDF2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,27 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello World"); } }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AuthService auth = new AuthService();
+
+        System.out.println("--- REGISTRO DE USUARIO (PASO 2) ---");
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Contraseña: ");
+        String pass = sc.nextLine();
+
+        if (auth.validatePasswordPolicy(pass)) {
+            try {
+                byte[] salt = auth.generateSalt();
+                String hash = auth.hashPassword(pass, salt);
+                System.out.println("\n[EXITO] Usuario registrado.");
+                System.out.println("Tu contraseña en la DB se vería así: " + hash);
+            } catch (Exception e) {
+                System.out.println("Error en el proceso de seguridad.");
+            }
+        } else {
+            System.out.println("\n[ERROR] La contraseña debe tener mín. 8 caracteres, una mayúscula y un número.");
+        }
+    }
+}
