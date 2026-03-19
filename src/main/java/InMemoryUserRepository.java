7b57e9235f121fc5ab4eaf976a62c276eec26667
BrayaMDev <cg4463380@gmail.com>
2026-03-18 19:51:31 -0600

    feat: implementar UserRepository en memoria

--- src/main/java/InMemoryUserRepository.java
+++ src/main/java/InMemoryUserRepository.java
@@ -1,3 +1,18 @@
-public class InMemoryUserRepository {
-    
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class InMemoryUserRepository implements UserRepository {
+
+    private final Map<Email, User> storage = new HashMap<>();
+
+    @Override
+    public void save(User user) {
+        storage.put(user.getEmail(), user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(Email email) {
+        return Optional.ofNullable(storage.get(email));
+    }
 }
