62fef2ada844506749a7bc5f75452b1229b2ad2d
Daniel <dan470074@gmail.com>
2026-03-18 18:36:39 -0600

    Se creo la consola basica del menú

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        System.out.println("--- BIENVENIDO ---");
+
+        while (running) {
+            System.out.println("\nSeleccione una opción:");
+            System.out.println("1 Register");
+            System.out.println("2 Login");
+            System.out.println("3 Exit");
+            System.out.print("> ");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    System.out.println("Iniciando registro...");
+                    
+                    break;
+                case "2":
+                    System.out.println("Iniciando sesión...");
+                   
+                    break;
+                case "3":
+                    System.out.println("Saliendo del sistema ¡Adiós!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida, intenta de nuevo.");
+            }
+        }
+        scanner.close();
     }
 }
