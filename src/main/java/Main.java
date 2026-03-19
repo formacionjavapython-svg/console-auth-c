2c35c51dff5ae1b72991829a6fd5f6557095b6a9
Tania Paola Mendez Quijano <taniamendezq23@gmail.com>
2026-03-18 18:44:26 -0600

    chore: add Main.java and project structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,5 @@
-public class Main { public static void main(String[] args) { System.out.println("Hello World"); } }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, el setup de mi rama está listo");
+    }
+}
