517ee590110310220ae7cad9dcef1d364198d500
BrayaMDev <cg4463380@gmail.com>
2026-03-18 20:10:06 -0600

    feat: agregar regla ContainsNumberRule para validar con numeros en la contraseña

---
+++ src/main/java/ContainsNumberRule.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContainsNumberRule implements PasswordRule {
+    @Override
+    public List<String> validate(String password, Email email) {
+        List<String> violations = new ArrayList<>();
+        if (!password.matches(".*\\d.*")) {
+            violations.add("La contraseña debe contener al menos un número.");
+        }
+        return violations;
+    }
+}
