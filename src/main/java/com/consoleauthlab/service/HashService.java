3dae7140e993868f5cd5261cede7ce75db5c01d3
francisco.abad <crip.damcad130@secihti.mx>
2026-03-18 23:18:25 -0600

    refactor: :zap: ajusta checkstyle

--- src/main/java/com/consoleauthlab/service/HashService.java
+++ src/main/java/com/consoleauthlab/service/HashService.java
@@ -50,7 +50,9 @@ public class HashService {
         StringBuilder hexString = new StringBuilder();
         for (byte b : bytes) {
             String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) hexString.append('0');
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
             hexString.append(hex);
         }
         return hexString.toString();
